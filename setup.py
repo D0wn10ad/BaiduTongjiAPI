8598509ecf65f1df725efd69a4dd4cf401cf6fa7
D0wn10ad <>
2026-09-07 08:57:42 +0800

    chore: bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def make_email(prefix, domain):
 
 setuptools.setup(
 	name = 'baidutongji',
-	version = '0.1.6',
+	version = '0.1.7',
 	author = 'JeffersonQin',
 	author_email = make_email('1247006353', 'qq.com'),
 	packages = ['baidutongji'],
